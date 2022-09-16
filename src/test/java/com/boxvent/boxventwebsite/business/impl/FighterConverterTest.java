3a5f0a8044cb4c748d59a48c2a1cbb9e3e818085
VladDumitru06 <vlad.dumitru06@gmail.com>
2022-09-16 17:46:10 +0200

    Added yaml file and one test for CI/CD integration

---
+++ src/test/java/com/boxvent/boxventwebsite/business/impl/FighterConverterTest.java
@@ -0,0 +1,18 @@
+package com.boxvent.boxventwebsite.business.impl;
+
+import com.boxvent.boxventwebsite.domain.Fighter;
+import com.boxvent.boxventwebsite.presistence.entity.FighterEntity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FighterConverterTest {
+@Test
+    public void convertedFighterShouldntChangeItsData_AfterConversion(){
+    FighterEntity fighter = FighterEntity.builder().id(1L).name("Vlad the boxer").record("15-2-0").build();
+    Fighter convertedFighter = FighterConverter.convert(fighter);
+    assertEquals(fighter.getName(),convertedFighter.getName());
+    assertEquals(fighter.getId(),convertedFighter.getId());
+    assertEquals(fighter.getRecord(),convertedFighter.getRecord());
+}
+}
