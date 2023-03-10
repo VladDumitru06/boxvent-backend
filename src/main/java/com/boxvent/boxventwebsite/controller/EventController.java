72e924360ee7943afc097af309b838750f3c54f8
VladDumitru06 <vlad.dumitru06@gmail.com>
2023-03-10 05:22:42 +0100

    crossOrigin fix

--- src/main/java/com/boxvent/boxventwebsite/controller/EventController.java
+++ src/main/java/com/boxvent/boxventwebsite/controller/EventController.java
@@ -20,7 +20,7 @@ import java.io.InputStream;
 
 @RestController
 @RequestMapping("/events")
-@CrossOrigin(origins ={"http://https://boxvent-frontend.herokuapp.com/"})
+@CrossOrigin(origins ={"http://https://boxvent-frontend.herokuapp.com:3000"})
 @RequiredArgsConstructor
 public class EventController {
     private final CreateEventUseCase createEventUseCase;
