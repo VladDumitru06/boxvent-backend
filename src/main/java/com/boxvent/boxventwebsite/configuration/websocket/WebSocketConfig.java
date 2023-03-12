bc670639635d114b276214cb2a2b253693eede17
VladDumitru06 <vlad.dumitru06@gmail.com>
2023-03-12 18:13:52 +0100

    Update WebSocketConfig.java

--- src/main/java/com/boxvent/boxventwebsite/configuration/websocket/WebSocketConfig.java
+++ src/main/java/com/boxvent/boxventwebsite/configuration/websocket/WebSocketConfig.java
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // Do change the allowed origin to your own url/ip +port combo
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("https://boxvent-frontend.herokuapp.com/").withSockJS();
     }
 }
